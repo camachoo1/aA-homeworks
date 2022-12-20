c5d37a597665cf58aa4ecf18c76a25519ccbaa4a
“Omar <ocamacho812@gmail.com>
2022-12-19 16:49:52 -0800

    Implemented initialize, place_stones, valid_move?, and make_move methods

--- W4D2/lib/board.rb
+++ W4D2/lib/board.rb
@@ -2,20 +2,57 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      unless i == 6 || i == 13
+        4.times { cup << :stone }
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    curr_idx = start_pos
+    until stones.empty?
+      curr_idx += 1
+      curr_idx = 0 if curr_idx > 13
+
+      if curr_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif curr_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[curr_idx] << stones.pop
+      end
+    end
+    render
+    next_turn(curr_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
+
   end
 
   def render
